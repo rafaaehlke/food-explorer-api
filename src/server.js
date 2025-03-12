1a38fac0108026f07cd29c760b9dfa02b828f93f
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-12 03:12:52 -0300

    :goal_net: Tratamento de erros

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
 const express = require("express"); 
 
 const routes = require("./routes"); 
@@ -5,6 +8,24 @@ const routes = require("./routes");
 const app = express(); 
 app.use(express.json()); 
 app.use(routes); 
+
+app.use((error, request, response, next) => {
+  // erro gerado pelo cliente
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error)
+
+  // erro gerado pelo servidor
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  })
+});
 
 
 const PORT = 3333; 
