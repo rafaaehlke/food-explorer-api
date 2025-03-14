f823de7b6dab7fb53fe56bd988f2d6dfb5fe4773
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-13 21:51:41 -0300

    :construction: Atualizaçao User

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -7,17 +7,50 @@ class UsersController {
     const { name, email, password } = request.body; 
 
     const database = await sqliteConnection();
-    const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email]) 
-    const passwordHashed = await hash(password, 8)
+    const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email]); 
+    const passwordHashed = await hash(password, 8);
 
     if(checkUserExist) {
       throw new AppError("Usuário já existente")
     }
 
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [ name, email, passwordHashed])
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [ name, email, passwordHashed]);
 
-    return response.status(201).json()
+    return response.status(201).json();
   } 
+
+  async update(request, response) {
+   const { name, email } = request.body;
+   const { id } = request.params;
+
+   const database = await sqliteConnection();
+   const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+   if(!user){
+     throw new AppError("Cadastro não encontrado.");
+   }
+
+   const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+   if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+     throw new AppError("Este email já está em uso.")
+   }
+
+   user.name = name;
+   user.email = email;
+
+
+   await database.run(`
+       UPDATE users SET 
+       name = ?, 
+       email = ?, 
+       updated_at = ? 
+       WHERE id = ?`,
+        [user.name, user.email, new Date(), id]
+
+     )
+     return response.status(200).json();
+  }
 };
 
 module.exports = UsersController;
