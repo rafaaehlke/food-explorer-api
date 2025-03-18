ad577e8519b2d9a1baf0aa0c0222e1f9f6ffdfc9
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-18 20:57:22 -0300

    :construction: Controller e Rotas para Dishes

--- src/controllers/DishController.js
+++ src/controllers/DishController.js
@@ -5,7 +5,7 @@ class DishController {
     const { image, name, category, price, description, ingredients } = request.body
     const { user_id } = request.params;
 
-    const [ dishes_id ] = await knex("dishes").insert({
+    const [dishes_id] = await knex("dishes").insert({
       user_id,
       image,
       name,
@@ -19,14 +19,45 @@ class DishController {
         dishes_id,
         name
       }
-    })
+    });
 
-    await knex("ingredients").insert(ingredientsInsert)
+    await knex("ingredients").insert(ingredientsInsert);
 
-    return response.status(201).json({ 
+    return response.status(201).json({
       message: 'Prato cadastrado com sucesso.'
     });
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const dishes = await knex("dishes").where({ id }).first();
+    const category = await knex("ingredients").where({ dishes_id: id }).orderBy("name");
+
+    return response.json({
+      ...dishes,
+      category
+    });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("dishes").where({ id }).delete();
+
+    return response.json({
+      message: "Prato excluido."
+    });
+  }
+
+  async index(request, response){
+    const { user_id } = request.query;
+
+    const dishes = await knex("dishes").where({ user_id }).orderBy("name")
+
+    return response.json(dishes)
+  }
+
 }
 
 module.exports = DishController
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -5,7 +5,12 @@ const dishRoutes = Router();
 const DishController = require("../controllers/DishController");
 const dishController = new DishController();
 
+dishRoutes.get("/", dishController.index); 
 dishRoutes.post("/:user_id", dishController.create); 
+dishRoutes.get("/:id", dishController.show); 
+dishRoutes.delete("/:id", dishController.delete); 
+
+
 
 
 module.exports = dishRoutes; 
