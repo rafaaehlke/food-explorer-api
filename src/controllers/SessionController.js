8dd2ee93391f02af28693eeed83324150ff99a14
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-20 20:53:19 -0300

    :construction: Controller para session, recebendo email e appError

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,18 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
+class SessionController{
+  async create(request, response){
+    const { email, password } = request.body;
+
+    const user = await knex("users").where({email}).first();
+
+    if(!user){
+      throw new AppError("Email e/ou senha incorretos.", 401);
+    }
+
+    return response.json({user});
+  }
+}
+
+module.exports = SessionController;
