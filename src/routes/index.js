f585ee91ee0b0510a2c6520477a61e0c3550d3f2
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-20 20:52:16 -0300

    :construction: session routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const dishesRouter = require("./dishes.routes");
+const sessionRouter = require("./session.routes");
 
 
 const routes = Router(); 
 
+routes.use("/session", sessionRouter);
 routes.use("/users", usersRouter); 
 routes.use("/dishes", dishesRouter); 
 
---
+++ src/routes/session.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express"); 
+
+
+const SessionController = require("../controllers/SessionController");
+const sessionController = new SessionController();
+
+
+const sessionRoutes = Router(); 
+sessionRoutes.post("/", sessionController.create); 
+
+module.exports = sessionRoutes;
