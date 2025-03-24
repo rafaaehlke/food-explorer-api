2c3baab43d361b1101d1048008e8e76df02e293b
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-24 20:48:20 -0300

    :bug: correcao alguns erros no MulterConfig

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -2,17 +2,17 @@ const multer = require("multer");
 const crypto = require("crypto");
 const path = require("path");
 
-const TMP_FOLDER = path.resolve("..", "..", "tmp");
-const UPLOAD_FOLDER = path.resolve(__dirname, "upload");
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+const UPLOAD_FOLDER = path.resolve(TMP_FOLDER, "upload");
 
 const MULTER = {
   storage: multer.diskStorage({
     destination: TMP_FOLDER,
     filename(request, file, callback){
       const fileHash = crypto.randomBytes(10).toString("hex");
-      const fileName = `${fileHas}-${file.originalname}`;
+      const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, this.filename);
+      return callback(null, fileName);
     }
   })
 };
