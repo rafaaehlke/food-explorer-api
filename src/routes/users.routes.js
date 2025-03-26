09c20ee7c0876ad2d8114d2a7426dc3263673aea
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-25 23:48:59 -0300

    :ok_hand:

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,21 +1,12 @@
 const { Router } = require("express"); 
-const multer = require("multer");
 const ensureAuthenticator = require("../middlewares/ensureAuthenticator");
-const uploadConfig = require("../configs/upload");
 
 const usersRoutes = Router(); 
-const upload = multer(uploadConfig.MULTER);
 
 const UsersController = require("../controllers/UsersController");
 const usersController = new UsersController();
 
 
 usersRoutes.post("/", usersController.create); 
-usersRoutes.put("/", ensureAuthenticator, usersController.update); 
-usersRoutes.patch("/pictureDish", ensureAuthenticator, upload.single("pictureDish"), (request, response) => {
-  console.log("imagem enviada", request.file.filename);
-
-  response.json()
-})
-
+usersRoutes.put("/", ensureAuthenticator, usersController.update);
 module.exports = usersRoutes; 
