d8523cd42f4a1151a6e95283b30cb6e45bbd2c64
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-25 23:47:56 -0300

    :construction: rotas para atualizacao dishes

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -7,12 +7,17 @@ const DishController = require("../controllers/DishController");
 const dishController = new DishController();
 
 
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+const upload = multer(uploadConfig.MULTER);
+
+
 dishRoutes.use(ensureAuthenticator);
 dishRoutes.get("/", dishController.index); 
 dishRoutes.post("/", dishController.create); 
 dishRoutes.get("/:id", dishController.show); 
 dishRoutes.delete("/:id", dishController.delete); 
-
+dishRoutes.patch("/:id/pictureDish", ensureAuthenticator, upload.single("pictureDish"), dishController.update)
 
 
 
