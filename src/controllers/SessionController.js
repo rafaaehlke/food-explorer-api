2fb7897b7843ca79fe15715105ed4d12a13fdc95
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-04-01 21:00:18 -0300

    :construction: recuperado role nas requisicoes

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -21,7 +21,7 @@ class SessionController {
     }
 
     const { secret, expiresIn } = authConfig.jwt
-    const token = sign({}, secret, {
+    const token = sign({ role: user.role }, secret, {
       subject: String(user.id),
       expiresIn
     })
