8ecf1401e602e3913de3f9d9848877230d52586b
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-06-24 21:15:28 -0300

    :heavy_plus_sign: seeds dependency

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,9 @@ module.exports = {
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
+      seeds: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "seeds"),
+    },
     useNullAsDefault: true
   }
 };
